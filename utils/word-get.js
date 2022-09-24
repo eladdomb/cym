136eedd4f0f4d121a29ca961a3d28302cb6a5cc8
eladdomb <eladdomb@gmail.com>
2022-09-24 23:43:46 +0200

    support from & to

--- utils/word-get.js
+++ utils/word-get.js
@@ -4,57 +4,54 @@ const Redis = require("../utils/redis");
 const API_KEY = "8e2cc95880834c2c8ad4449ce05feef5";
 const API_HOST = "newsapi.org";
 const API_PATH =
-	"/v2/everything?language=en&pageSize=100&sortBy=publishedAt&sources=bbc-news&apiKey=";
+  "/v2/everything?language=en&pageSize=100&sortBy=publishedAt&sources=bbc-news&apiKey=";
 
 const options = {
-	host: API_HOST,
-	path: API_PATH + API_KEY,
-	headers: {"User-Agent": "Mozilla/5.0"},
+  host: API_HOST,
+  path: API_PATH + API_KEY,
+  headers: { "User-Agent": "Mozilla/5.0" },
 };
 
 const WordGet = {};
 
-
 WordGet.run = async function (from, to) {
-	return new Promise(function (resolve, reject) {
+  return new Promise(function (resolve, reject) {
+    function stripNonWordChars(str) {
+      return str.replace(/[^a-z ]/gi, "");
+    }
 
-		function stripNonWordChars(str) {
-			return str.replace(/[^a-z ]/gi, "");
-		}
+    function listWords(str) {
+      return str.split(" ");
+    }
 
-		function listWords(str){
-			return str.split(" ");
-		}
+    const callback = function (response) {
+      let str = "";
 
-		const callback = function (response) {
-			let str = "";
+      response.on("data", function (chunk) {
+        str += chunk;
+      });
 
-			response.on("data", function (chunk) {
-				str += chunk;
-			});
-
-			response.on("end", function () {
-				try {
-					const data = JSON.parse(str);
-					const articles = data.articles;
-					let wordsList = [];
-					for (let article of articles) {
-						const title = stripNonWordChars(article.title);
-						wordsList = wordsList.concat(listWords(title));
-						const description = stripNonWordChars(article.description);
-						wordsList = wordsList.concat(listWords(description));
-					}
-					resolve(wordsList);
-				} catch (e) {
-					reject();
-				}
-			});
-		};
-		Http.request(options, callback).end();
-
-	});
-
-
+      response.on("end", function () {
+        try {
+          const data = JSON.parse(str);
+          const articles = data.articles;
+          let wordsList = [];
+          for (let article of articles) {
+            const title = stripNonWordChars(article.title);
+            wordsList = wordsList.concat(listWords(title));
+            const description = stripNonWordChars(article.description);
+            wordsList = wordsList.concat(listWords(description));
+          }
+          resolve(wordsList);
+        } catch (e) {
+          reject();
+        }
+      });
+    };
+    const myoptions = Object.assign({}, options);
+    myoptions.path = myoptions.path += "&from=" + from + "&to=" + to;
+    Http.request(myoptions, callback).end();
+  });
 };
 
 module.exports = WordGet;
